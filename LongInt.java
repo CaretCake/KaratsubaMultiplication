5c3b07b40ed330e1fad33dc9167d380e2db48958
CaretCake <caretcake@outlook.com>
2019-02-23 01:30:02 -0500

    Changed numberArray implementation, added getLongConversion() method.

--- LongInt.java
+++ LongInt.java
@@ -2,12 +2,30 @@ import java.util.ArrayList;
 
 public class LongInt {
   int numOfDigits;
-  ArrayList<Integer> numberArray = new ArrayList<Integer>();
+  ArrayList<String> numberArray = new ArrayList<String>();
 
   public LongInt (String numberString) {
     this.numOfDigits = numberString.length();
     for (int i = 0; i < numberString.length(); i++) {
-      numberArray.add(numberString.charAt(i) - '0');
+      numberArray.add(numberString.charAt(i));
     }
+  }
+
+  public long getLongConversion() throws longIntConversionException {
+    if (this.numOfDigits > 9) {
+      throw new longIntConversionException("LongInt is too large to convert at " + this.numOfDigits + " digits.");
+    } else {
+      String numberString = "";
+      for (char digit : numberArray)  {
+        numberString += digit;
+      }
+      return Long.parseLong(numberString);
+    }
+  }
+}
+
+class longIntConversionException extends Exception {
+  public AlsCustomException(String message) {
+    super(message);
   }
 }
